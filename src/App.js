082441c69edc3aaa6c92962f86f5d23f7cf043d9
Kyaw <khtin001@gmail.com>
2018-07-28 13:26:38 +1000

    Added glamorous, updated package.json to reflect packages. App can now be dragged

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from "react";
-import MapGL from "react-map-gl";
+import MapGL, { Marker } from "react-map-gl";
+import glamorous from "glamorous";
 require("dotenv").config();
 
+const POI = glamorous.div({
+  borderRadius: "50px",
+  width: "50px",
+  height: "50px",
+  backgroundColor: "red"
+});
+
 class App extends Component {
+  state = {
+    viewport: {
+      width: 1000,
+      height: 500,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <MapGL
-          width={1000}
-          height={500}
-          latitude={37.768}
-          longitude={-122.331}
-          zoom={9.017}
-          mapStyle="mapbox://styles/khtin/cjk46r5ap15nm2smtyidxl1hb"
+          {...this.state.viewport}
+          mapStyle="mapbox://styles/khtin/cjk46q8hh4fen2sqz98wylqrd"
           mapboxApiAccessToken={process.env.REACT_APP_MapboxAccessToken}
-        />
+          onViewportChange={viewport => this.setState({ viewport })}
+        >
+          <Marker
+            latitude={37.78}
+            longitude={-122.41}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <POI>TRENDDDING!!!🔥🔥🔥🔥🔥🔥🔥🔥</POI>
+          </Marker>
+        </MapGL>
       </div>
     );
   }
